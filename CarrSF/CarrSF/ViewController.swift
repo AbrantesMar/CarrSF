c1fe09250df9c8f1b35b68b6dfe89ac6318b8482
Márcio Abrantes <abs.marcio@gmail.com>
2023-06-13 17:53:23 -0300

    start struct from project

---
+++ CarrSF/CarrSF/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarrSF
+//
+//  Created by Márcio Abrantes on 13/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CarrSF
-Desafio para consumir API do github em Swift
+ Desafio para consumir API do github em Swift
+
+## Pre requisito para rodar o app
+ Swift 5, xCode 14.3, CocoaPods, Brew
+
